4b9d4a697ef0559cc4899ab46d54b85b539d4476
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-11-15 22:30:43 -0600

    added some style, images, and psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//  click a div to get a number
+//  display number picked 
+//  display number sum total
+//  display wins
+//  display losses
+//  start game over and over keeping tallies of wins and losses
+
+
+
