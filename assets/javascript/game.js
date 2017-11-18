c30093132c9e7b8d3cbc7260f1f128a3f6e56c8f
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-11-17 21:18:15 -0600

    added some variables and random number generation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,37 +5,50 @@
 //  display losses
 //  start game over and over keeping tallies of wins and losses
 
+
+var targetNum = Math.floor(Math.random() * (120-19+1)) + 19;
+console.log(targetNum);
+// $('#number').append(targetNum);
+
+var counter = 0;
+var wins = 0;
+var losses = 0;
+
+
 $(document).ready(function(){
 
   // reveal the Cartmans
-  $(".gems").on("click", function() {
+  $(".startBtn").on("click", function() {
     $(".gems").animate({ top: "50px", right: "500px" }, "fast");
     $(".gems").empty();
+    $(".startBtn").addClass('display', 'none');
+    console.log("clicked?");
     
+    //maybe pick random image on restart?
     var imgArray = ['assets/images/Cartman.jpg', 'assets/images/magicCartman.png', 'assets/images/normalCartman.png', 'assets/images/sadCartman.png']
 
     for (var i=0; i<imgArray.length; i++) {
+      var randomNum = Math.floor(Math.random() * (12-1+1)) + 1;
       var imgCartman = $('<img>');
-      $(imgCartman).addClass('cartman-image');
+      $(imgCartman).addClass('cartman-button');
       $(imgCartman).attr('src', imgArray[i]);
       $('.gems').append(imgCartman);
       $('.gems').css("background-image","none");
       $('.gems').css("background-color","transparent");
       $('.gems').css("height","250px");
+      $(imgCartman).attr('value', randomNum);
+
     }
   });
+ 
+  $('.cartman-button').on('click', function () {
+    console.log("working")
+    // counter += $(this).val();
+    // console.log(counter);
+  });
 
-  var targetNum = Math.floor(Math.random() * (120-19+1)) + 19;
-  console.log(targetNum);
-  // $('#number').append(targetNum);
-
-  var counter = 0;
-  var wins = 0;
-  var losses = 0;
-
-  $(".cartman-image").on("click", function() {
-    var getNum = Math.floor(Math.random() * (12-1+1)) + 19;
-    console.log(getNum);
+  // click the cartmans
+  $(".cartman-button").on("click", function() {
     console.log('worked');
   });
 
