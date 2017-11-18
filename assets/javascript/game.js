f1c350d421bcd467021283217adbbf493039705f
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-11-18 14:39:50 -0600

    trying some things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,37 +18,69 @@ $(document).ready(function(){
 
   var counter = 0;
   var targetNum = Math.floor(Math.random() * (120-19+1)) + 19;
-  $('#number').append(targetNum);
+  $('#number').html(targetNum);
 
   // reveal the Cartmans
   $(".gems").one("click", function() {
     $(".gems").animate({ top: "50px", right: "500px" }, "fast");
     $(".gems").empty();
     $(".startBtn").addClass('display', 'none');
-    console.log("clicked?");
+    randomCartman();
     
-    //maybe pick random image on restart?
-    var imgArray = ['assets/images/Cartman.jpg', 'assets/images/magicCartman.png', 'assets/images/normalCartman.png', 'assets/images/sadCartman.png']
+    // Thinking about maybe picking random image on restart?
 
-    for (var i=0; i<imgArray.length; i++) {
-      var randomNum = Math.floor(Math.random() * (12-1+1)) + 1;
-      var imgCartman = $('<img>');
-      $(imgCartman).addClass('cartman-button');
-      $(imgCartman).attr('src', imgArray[i]);
-      $('.gems').append(imgCartman);
-      $('.gems').css("background-image","none");
-      $('.gems').css("background-color","transparent");
-      $('.gems').css("height","250px");
-      $(imgCartman).attr('value', randomNum);
+    function randomCartman(){
+      var imgArray = ['assets/images/Cartman.jpg', 'assets/images/magicCartman.png', 'assets/images/normalCartman.png', 'assets/images/sadCartman.png']
+      
+      for (var i=0; i<imgArray.length; i++) {
+        var randomNum = Math.floor(Math.random() * (12-1+1)) + 1;
+        var imgCartman = $('<img>');
+        $(imgCartman).addClass('cartman-button');
+        $(imgCartman).attr('src', imgArray[i]);
+        $('.gems').append(imgCartman);
+        $('.gems').css("background-image","none");
+        $('.gems').css("background-color","transparent");
+        $('.gems').css("height","250px");
+        $(imgCartman).attr('value', randomNum);
+      }
     }
- 
+
     $('.cartman-button').click(function () {
       var cartmanValue = $(this).attr('value');
       cartmanValue = parseInt(cartmanValue);
       counter += cartmanValue;
-      console.log(counter);
       $('#score').text(counter);
+      checkWin();
+      checkLoss();
     });
+
+    function checkWin() {
+      if (counter === targetNum){
+        alert("you win");
+        wins++;
+        $('#wins').html("Wins: " + wins);
+        Restart();
+      }
+    }
+
+    function checkLoss() {
+      if (counter > targetNum){
+        alert("you lose");
+        losses++;
+        $('#losses').html("Losses: " + losses);
+        Restart();
+      }
+    }
+
+    function Restart() {
+      //$('.gems').empty();
+      randomCartman();
+      var counter = 0;
+      $('#score').text(counter);
+      var targetNum = Math.floor(Math.random() * (120-19+1)) + 19;
+      $('#number').html(targetNum);
+    }
+
   });
 });
 
