9562a3386ac2d0c05c1c8b7fe26f7ad91868f2db
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-11-21 22:55:59 -0600

    added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,29 @@
 //  CLICK-A-CARTMAN GAME
 
+//  Declare wins and losses
 var wins = 0;
 var losses = 0;
 
 
 $(document).ready(function(){
 
+  //  Declare Score Counter and Target Number
   var counter = 0;
   var targetNum = Math.floor(Math.random() * (120-19+1)) + 19;
   $('#number').html(targetNum);
 
+  // Fancy open div animation for Cartmans to populate
   $(".gems").one("click", function() {
     $(".gems").animate({ top: "50px", right: "500px" }, "slow");
     $(".gems").empty();
     startGame();
   });
 
+    //  Let the Games Begin
     function startGame() {
       var imgArray = ['assets/images/Cartman.jpg', 'assets/images/magicCartman.png', 'assets/images/normalCartman.png', 'assets/images/sadCartman.png']
 
+      //  Loop through image aray, and set up Cartman images
       for (var i=0; i<imgArray.length; i++) {
         var randomNum = Math.floor(Math.random() * (12-1+1)) + 1;
         var imgCartman = $('<img>');
@@ -26,15 +31,13 @@ $(document).ready(function(){
         $(imgCartman).attr('src', imgArray[i]);
         $(imgCartman).attr('value', randomNum);
         $('.gems').append(imgCartman);
-        // CSS class variable?
-        //$('.gems').css('.changeup');
         $('.gems').css("background-image","none");
         $('.gems').css("background-color","transparent");
         $('.gems').css("height","250px");
         $('.gems').css("width", "90%"); 
-        // $('.gems').css("width", "1500px"); 
       }
     
+      //  Every Click Counts
       $('.cartman-button').click(function () {
         var cartmanValue = $(this).attr('value');
         cartmanValue = parseInt(cartmanValue);
@@ -45,9 +48,9 @@ $(document).ready(function(){
       });
     }
 
+    //  Did the User win?
     function checkWin() {
       if (counter === targetNum){
-        // alert("you win: " + counter + " = " + targetNum);
         wins++;
         $('#wins').html("Wins: " + wins);
         $('#show').empty();
@@ -64,12 +67,11 @@ $(document).ready(function(){
       }
     }
 
+    //  Did the user lose?  Does that mean he/she is a lusr?
     function checkLoss() {
       if (counter > targetNum){
-        // alert("you lose: " + counter + " > " + targetNum);
         losses++;
         $('#losses').html("Losses: " + losses);
-        $('#show').empty();
         if (losses === 3 || losses === 6){
           var weak = new Audio("http://www.shannon-g.net/fun/multimedia/sounds/sp/oh_weak.wav");
           weak.play();
@@ -88,6 +90,7 @@ $(document).ready(function(){
       }
     }
 
+    //  Can I get a reset?  Reset on aisle 9!!
     function Reset() {
       counter = 0;
       $('#score').text(counter);
@@ -97,12 +100,14 @@ $(document).ready(function(){
       startGame();
     }
 
+    //  It just got weird
     function hankey() {
       var imgHankey = $('<img>');
       $(imgHankey).attr('src', "https://i.imgur.com/4X9Q9.jpg")
       $('#show').append(imgHankey);
     }
 
+    //  This sound is extra special for no reason at all, but just put all the sounds up above where needed
     function beefCake() {
       var audio = new Audio("http://www.thesoundarchive.com/spsounds/BeefCake-2.mp3");
       audio.play();
